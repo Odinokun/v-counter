53242195296f132654336a8ea5d93cd239b3bfcb
odinokun <odinokun@gmail.com>
2024-12-05 16:20:16 -0500

    feat: enhance Button and Counter components with state management and disabled functionality

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,15 +1,20 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Button } from './Button';
 
 export const Counter: FC = () => {
+  const [counter, setCounter] = useState<number>(0);
+
+  const incCount = () => setCounter(prevState => prevState + 1);
+  const resetCount = () => setCounter(0);
+
   return (
     <div className='block'>
       <div className='block__body block--border'>
-        <h1 className='count-val'>0</h1>
+        <h1 className='count-val'>{counter}</h1>
       </div>
       <div className='block__footer  block--border'>
-        <Button name='Inc' onClick={() => {}} />
-        <Button name='Reset' onClick={() => {}} />
+        <Button name='Inc' onClick={incCount} />
+        <Button name='Reset' onClick={resetCount} disabled={!counter} />
       </div>
     </div>
   );
